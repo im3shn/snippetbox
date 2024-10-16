9c9cfb0702d72b96c79d808483711a2dc82970af
Thrishan Anbazhagan <im3shn@gmail.com>
2024-10-16 12:06:51 +0530

    snippetCreatePost db save in handler and model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -41,10 +41,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+    w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
+    title := "O Snail"
+    content := "O Snail \nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+    expires := 7
+
+    id, err := app.snippets.Insert(title, content, expires)
+
+    if err != nil {
+        app.serverError(w, r, err)
+    }
+
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
