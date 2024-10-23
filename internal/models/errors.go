70b081c8e526ee9fe36b1ece144f5213d9fa07a6
Thrishan Anbazhagan <im3shn@gmail.com>
2024-10-23 22:15:42 +0530

    creating users model and making available to application

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,8 @@ import (
 	"errors"
 )
 
-var ErrNoRecord = errors.New("models: No Matching Record Found")
+var (
+	ErrNoRecord           = errors.New("models: No Matching Record Found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
